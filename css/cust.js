08568b6353c4c7e4e82be5e0c6658749b396de26
rayroman1 <roman4756@yahoo.com>
2022-07-06 16:58:55 -0400

    upload Html Form

---
+++ css/cust.js
@@ -0,0 +1,9 @@
+function showtext(){
+
+var name = document.getElementById('email');
+var pasw = document.getElementById('pwd');
+var psr = document.getElementById('pwd-repeat');
+
+document.getElementById("show").innerHTML
+= email+"<br>"+pwd+"<br>"+pwd-repeat;
+}
